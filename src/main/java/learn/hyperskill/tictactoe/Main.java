0f577168e55c62608d729605abe648e03dd01c5a
Denis Popkov <pkvdenis@gmail.com>
2019-11-23 14:38:40 +0300

    TicTacToe: refactor Main

--- src/main/java/learn/hyperskill/tictactoe/Main.java
+++ src/main/java/learn/hyperskill/tictactoe/Main.java
@@ -1,28 +1,56 @@
 package learn.hyperskill.tictactoe;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     private static final String NL = System.lineSeparator();
-    private static final int WIDTH = 3;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
+        char[][] field = parseInput(input);
+        System.out.print(buildOutput(field));
+    }
+
+    private static char[][] parseInput(String input) {
+        int size = (int) Math.sqrt(input.length());
+        char[][] chars = new char[size][size];
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < input.length(); i++) {
+            chars[row][col] = input.charAt(i);
+            col++;
+            if (col == size) {
+                col = 0;
+                row++;
+            }
+        }
+        return chars;
+    }
+
+    private static String buildOutput(char[][] field) {
         StringBuilder builder = new StringBuilder();
-        builder.append("---------");
+        String horizontal = makeLine(field[0].length * 2 + 3);
+        builder.append(horizontal);
         builder.append(NL);
-        for (int offset = 0; offset < input.length(); offset += WIDTH) {
+        for (char[] row : field) {
             builder.append("| ");
-            for (int j = 0; j < WIDTH; j++) {
-                builder.append(input.charAt(offset + j));
+            for (char ch : row) {
+                builder.append(ch);
                 builder.append(' ');
             }
             builder.append("|");
             builder.append(NL);
         }
-        builder.append("---------");
+        builder.append(horizontal);
         builder.append(NL);
-        System.out.print(builder.toString());
+        return builder.toString();
+    }
+
+    private static String makeLine(int length) {
+        char[] chars = new char[length];
+        Arrays.fill(chars, '-');
+        return new String(chars);
     }
 }
