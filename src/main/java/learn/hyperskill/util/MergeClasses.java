91bc4db53d4bde992a56b7447518e724bf69d2a7
Denis Popkov <pkvdenis@gmail.com>
2020-11-25 17:03:35 +0300

    MergeClasses utility: processing imports and package lines.

--- src/main/java/learn/hyperskill/util/MergeClasses.java
+++ src/main/java/learn/hyperskill/util/MergeClasses.java
@@ -1,6 +1,9 @@
 package learn.hyperskill.util;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * This utility class helps to merge all java source code files into one class.
@@ -18,7 +21,7 @@ public class MergeClasses {
         try {
             String sourceDir = args[0];
             String targetFile = args[1];
-            System.out.printf("Merging files from %s to %s%n", sourceDir, targetFile);
+            System.out.printf("Merging files from folder %s to %s%n", sourceDir, targetFile);
             merge(new File(sourceDir), new File(targetFile));
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,25 +35,95 @@ public class MergeClasses {
                 System.err.println("This abstract path does not denote a directory " + sourceDir);
                 return;
             }
+            List<String> importLines = new ArrayList<>();
+            List<JavaFile> allJavaFiles = new ArrayList<>();
             for (File file : files) {
                 if (file.getName().endsWith(".java")) {
-                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                        // todo: find and store 'import' statements for moving them to the start
-                        reader.lines()
-                                .filter(line -> !line.startsWith("package "))
-                                .map(line -> line.startsWith("public ") ? line.substring(7) : line)
-                                .forEach(line -> {
-                                    try {
-                                        writer.write(line);
-                                        writer.write(NL);
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                });
-                    }
-                    writer.write(NL);
+                    List<String> oneFileLines = readOneFileLines(file, importLines);
+                    JavaFile javaFile = new JavaFile(file.getName(), oneFileLines);
+                    allJavaFiles.add(javaFile);
                 }
             }
+            importLines.sort(Comparator.naturalOrder());
+            allJavaFiles.sort(Comparator.comparing(JavaFile::getName));
+            makeMainFirst(allJavaFiles);
+            checkAndEnsurePackage(targetFile, importLines);
+
+            writeLines(writer, importLines);
+            for (JavaFile jf : allJavaFiles) {
+                writeLines(writer, jf.getLines());
+            }
+        }
+    }
+
+    private static void checkAndEnsurePackage(File targetFile, List<String> importLines) throws IOException {
+        String path = targetFile.getCanonicalPath();
+        final String pathInProject = "/src/main/java/";
+        int idx = path.indexOf(pathInProject);
+        if (idx != -1) {
+            int packageStart = idx + pathInProject.length();
+            int packageEnd = path.lastIndexOf(File.separator);
+            String packageStr = path.substring(packageStart, packageEnd);
+            String packageName = packageStr.replaceAll("/", ".");
+            importLines.add(0, "package " + packageName + ";" + NL);
+            importLines.add(1, NL);
+        }
+    }
+
+    private static void makeMainFirst(List<JavaFile> javaFiles) {
+        int mainIdx = -1;
+        for (int i = 0; i < javaFiles.size(); i++) {
+            JavaFile jf = javaFiles.get(i);
+            if ("Main.java".equals(jf.getName())) {
+                mainIdx = i;
+                break;
+            }
+        }
+        if (mainIdx != -1) {
+            JavaFile main = javaFiles.remove(mainIdx);
+            javaFiles.add(0, main);
+        }
+    }
+
+    private static void writeLines(BufferedWriter writer, List<String> lines) throws IOException {
+        for (String line : lines) {
+            writer.write(line);
+        }
+        writer.write(NL);
+    }
+
+    private static List<String> readOneFileLines(File file, List<String> importLines) throws IOException {
+        List<String> oneFileLines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            reader.lines()
+                    .filter(line -> !line.startsWith("package "))
+                    .map(line -> line.startsWith("public ") ? line.substring(7) : line)
+                    .forEach(line -> {
+                        if (line.startsWith("import ")) {
+                            importLines.add(line + NL);
+                        } else {
+                            oneFileLines.add(line + NL);
+                        }
+                    });
+        }
+        return oneFileLines;
+    }
+
+    private static class JavaFile {
+        private final String name;
+        private final List<String> lines;
+
+        public JavaFile(String name, List<String> lines) {
+            this.name = name;
+            this.lines = lines;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public List<String> getLines() {
+            return lines;
         }
     }
 }
